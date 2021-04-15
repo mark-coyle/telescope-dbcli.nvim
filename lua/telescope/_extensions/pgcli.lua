20b464fa11c08f68b1569ac8be606e03a6d18b7e
Mark Coyle <markcoyle14@hotmail.com>
2021-04-15 23:26:24 +0100

    Feat: Improve sorter
    Sort by date if no prompt is entered, otherwise best effort at matching
    the prompt literal to a query.

--- lua/telescope/_extensions/pgcli.lua
+++ lua/telescope/_extensions/pgcli.lua
@@ -80,6 +80,10 @@ end
 function M.pgcli_picker(opts)
   opts = opts or {}
   local results = get_queries()
+  local sorted_state = {
+    last_sorted_time = 0,
+    last_sorted_index = 0
+  }
 
   pickers.new(opts, {
     prompt_title = 'Pgcli History',
@@ -100,7 +104,49 @@ function M.pgcli_picker(opts)
         }
       end
     },
-    sorter = sorters.fuzzy_with_index_bias(),
+    --sorter = sorters.fuzzy_with_index_bias(),
+    sorter = sorters.new {
+      scoring_function = function(_, prompt, _, entry)
+        -- Basically, if no prompt is entered then default to sorting by the query timestamp
+        -- loading most recent queries first.
+        --
+        -- If a prompt is entered, then look for the passed in text, either an exact match, or
+        -- any characters from the prompt. The latter is a weak effort, it should probably try
+        -- to match N+ characters or at least half of the prompt perhaps ( :match() == prompt.lenght / 2 )
+        -- 
+        -- Or see if we can defer to one of the supported functions at that point instead
+        if prompt == "" then
+          local date = entry.display:match("%d+-%d+-%d+")
+
+          if date == nil then
+            return -1
+          end
+
+          local date_parts = vim.fn.split(date, "-")
+          local entry_time = os.time({ year = date_parts[1], month = date_parts[2], day = date_parts[3] })
+
+          if entry_time > sorted_state.last_sorted_time then
+            sorted_state.last_sorted_time = entry_time
+            sorted_state.last_sorted_index = sorted_state.last_sorted_index - 1
+
+            return sorted_state.last_sorted_index
+          end
+        else
+          -- TODO: this disables case sensitivity which may be desired...
+          if entry.query:lower():match(prompt:lower()) then
+            return 0
+          end
+
+          -- TODO: this disables case sensitivity which may be desired...
+          if entry.query:lower():match("["..prompt:lower().."]") == nil then
+            return -1
+          end
+
+          return 1
+        end
+      end
+    },
+
     previewer = previewers.display_content.new({}),
     attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function(_, _)
